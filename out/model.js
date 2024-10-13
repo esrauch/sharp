ae98f51403c285943d8f236d68005cdb568b2817
esrauch <esrauch@gmail.com>
2024-10-13 14:59:11 -0400

    .

--- out/model.js
+++ out/model.js
@@ -1,3 +1,24 @@
+const pathCache = new WeakMap();
+function cachedPath(el) {
+    const cached = pathCache.get(el);
+    if (cached)
+        return cached;
+    const p = new Path2D();
+    switch (el.type) {
+        case 'Rect':
+            p.rect(el.x, el.y, el.w, el.h);
+            break;
+        case 'Poly':
+            const xs = el.xs;
+            const ys = el.ys;
+            if (xs.length != ys.length)
+                throw `Poly with mismatched xs and ys`;
+            for (let i = 0; i < xs.length; ++i)
+                p.lineTo(xs[i], ys[i]);
+            break;
+    }
+    return p;
+}
 export class Model {
     constructor() {
         this.doc = {
@@ -23,7 +44,7 @@ export class Model {
         this.renderElement(ctx, this.doc.root);
     }
     renderElement(ctx, el) {
-        const mustSave = el.transform || (el.fillStyle !== ctx.fillStyle);
+        const mustSave = el.transform || el.fillStyle;
         if (mustSave)
             ctx.save();
         if (el.transform) {
@@ -32,13 +53,17 @@ export class Model {
         if (el.fillStyle)
             ctx.fillStyle = el.fillStyle;
         switch (el.type) {
-            case "Group":
+            case 'Group':
                 for (const child of el.elements)
                     this.renderElement(ctx, child);
                 break;
-            case "Rect":
-                ctx.fillRect(el.x, el.y, el.w, el.h);
+            case 'Rect':
+            case 'Poly':
+                const p = cachedPath(el);
+                ctx.fill(p);
                 break;
+            default:
+                console.error(`unhandled el ${el}`);
         }
         if (mustSave)
             ctx.restore();
--- src/model.ts
+++ src/model.ts
@@ -1,4 +1,3 @@
-
 export type Group = {
     type: 'Group'
     elements: Element[],
@@ -13,17 +12,45 @@ export type Rect = {
     h: number,
 }
 
+export type Poly = {
+    type: 'Poly',
+    xs: number[],
+    ys: number[],
+}
+
 export type Transformable = {
     transform?: DOMMatrix,
     fillStyle?: string,
 }
 
-export type Element = Transformable & (Group | Rect)
+export type Element = Transformable & (Group | Rect | Poly)
 
 export type Document = {
     root: Group,
     fg: string,
     bg: string,
+}
+
+const pathCache = new WeakMap<Element, Path2D>();
+
+function cachedPath(el: Rect | Poly) {
+    const cached = pathCache.get(el)
+    if (cached) return cached;
+
+    const p = new Path2D()
+    switch (el.type) {
+        case 'Rect':
+            p.rect(el.x, el.y, el.w, el.h)
+            break
+        case 'Poly':
+            const xs = el.xs
+            const ys = el.ys
+            if (xs.length != ys.length) throw `Poly with mismatched xs and ys`
+            for (let i = 0; i < xs.length; ++i)
+                p.lineTo(xs[i], ys[i])
+            break
+    }
+    return p
 }
 
 export class Model {
@@ -53,7 +80,7 @@ export class Model {
     }
 
     private renderElement(ctx: CanvasRenderingContext2D, el: Element) {
-        const mustSave = el.transform || (el.fillStyle !== ctx.fillStyle)
+        const mustSave = el.transform || el.fillStyle
 
         if (mustSave) ctx.save()
 
@@ -64,12 +91,16 @@ export class Model {
         if (el.fillStyle) ctx.fillStyle = el.fillStyle
 
         switch (el.type) {
-            case "Group":
+            case 'Group':
                 for (const child of el.elements) this.renderElement(ctx, child)
                 break
-            case "Rect":
-                ctx.fillRect(el.x, el.y, el.w, el.h)
+            case 'Rect':
+            case 'Poly':
+                const p = cachedPath(el)
+                ctx.fill(p)
                 break
+            default:
+                console.error(`unhandled el ${el}`)
         }
 
         if (mustSave) ctx.restore()
